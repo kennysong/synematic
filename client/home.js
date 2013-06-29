ed0e04b2236eabcb303857025f5086a1119b4285
Amey <amey.desai@utah.edu>
2013-06-29 17:54:12 -0600

    Routing

---
+++ client/home.js
@@ -0,0 +1,20 @@
+
+if (Meteor.isClient) {
+  Meteor.Router.add({
+    '/': 'roomPage',
+    '/upload': 'uploadPage'
+  })
+  
+  Template.body.helpers({
+    layoutName: function() {
+      switch (Meteor.Router.page()) {
+        case 'roomPage':
+          return 'roomLayout';
+        case 'uploadPage':
+          return 'uploadLayout';
+        default:
+          return 'roomLayout';
+      }
+    }
+  });
+}
