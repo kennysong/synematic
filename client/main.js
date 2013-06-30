7ff3bff4cc88f7f15d1469558a04ad304df3de50
Kenny Song <jellyksong@gmail.com>
2013-06-30 04:22:44 -0700

    added the sexy, jquery/css front page magic

--- client/main.js
+++ client/main.js
@@ -1,5 +1,5 @@
 var S3_URL = "https://s3-us-west-2.amazonaws.com/meteor-hackathon/";
-
+var ROOM_URL = "http://synematic.meteor.com/room/";
 
 if (Meteor.isClient) {
   Meteor.startup(function () {
@@ -12,14 +12,19 @@ if (Meteor.isClient) {
 
 
   // upload handler
-  Template.upload.events({
+  Template.home.events({
     'click #attachment' : function () {
-      filepicker.pickAndStore({},
+      if (!$('#attachment').hasClass('disabled')) {
+        filepicker.pickAndStore({},
           {location:"S3"}, function(InkBlobs){
             var key = InkBlobs[0]['key'];
             var url = S3_URL + key;
             console.log(JSON.stringify(url));
-      });
+            $('#uploadnotif').css({'visibility':'visible'});
+            $('#attachment').text('Success!');
+            $('#attachment').addClass('disabled');
+       });
+      }
     }
   });
 
@@ -28,11 +33,33 @@ if (Meteor.isClient) {
   }); 
 
   Template.home.events({
+    'click #submitroom':function() {
+      $('#steptwo').addClass('stepactive');
+      $('#create').animate({"left": "-=350px", "opacity":0}, '2000', 'linear', function(){$('#create').css({'visibility':'hidden'})});
+      $('#uploading').css({'visibility':'visible'});
+      $('#uploading').animate({"left": "-=297px", "opacity":1}, '2000', 'linear');
+
+      var roomId = encodeURIComponent($('#nameroom').val());
+      var url = ROOM_URL + roomId;
+      $('#url').val(url);
+    },
+
+    'click #afterupload':function() {
+      $('#steptwo').removeClass('stepactive');
+      $('#stepthree').addClass('stepactive');
+      $('#uploading').animate({"left": "-=350px", "opacity":0}, '2000', 'linear', function(){$('#uploading').css({'visibility':'hidden'})});
+      $('#share').css({'visibility':'visible'});
+      $('#share').animate({"left": "-=532px", "opacity":1}, '2000', 'linear');
+
+    },
+
+
     'focus #nameroom':function() {
       if (!$('#stepone').hasClass('stepactive')){
         $('#stepone').addClass('stepactive')
       }
     },
+
     'blur #nameroom': function() {
       $('#stepone').removeClass('stepactive')
     }
