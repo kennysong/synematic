af96c977207b3f133c41e7c9fb7d866f6f584e91
Kenny Song <jellyksong@gmail.com>
2013-06-29 18:11:17 -0700

    some routing stuff about page

--- client/routing.js
+++ client/routing.js
@@ -1,15 +1,25 @@
 if (Meteor.isClient) {
   Meteor.Router.add({
-    '/': 'roomPage',
+    '/': 'home',
+    '/about' : 'about',
+    '/room/:id' : function(id) {
+        alert(id)
+        return 'room'
+    }
+
   })
   
   Template.content.helpers({
     layoutName: function() {
       switch (Meteor.Router.page()) {
-        case 'roomPage':
-          return 'roomLayout';
+        case 'home':
+          return 'homeLayout';
+        case 'about':
+          return 'aboutLayout';
+        case 'room':
+          return 'roomLayout'
         default:
-          return 'roomLayout';
+          return 'homeLayout';
       }
     }
   });
